39dc6d6ab49af777acd6fa9707b8ca03ea3ed257
Marcos <colomamarcos777@gmail.com>
2026-04-13 19:35:08 -0300

    refactor(head): modularizar layout con partials

--- app.js
+++ app.js
@@ -11,35 +11,35 @@ app.use(express.static("assets"));
 //--------------Rutas-----------------//
 
 app.get("/", (req, res) => {
-    res.render("pages/home");
+    res.render("pages/home"), {title:"Home"};
 });
 
 app.get("/cart", (req, res) => {
-    res.render("pages/cart");
+    res.render("pages/cart"), {title:"Cart"};
 });
 
 app.get("/checkout", (req, res) => {
-    res.render("pages/checkout");
+    res.render("pages/checkout"), {title:"Checkout"};
 });
 
 app.get("/menu", (req, res) => {
-    res.render("pages/menu");
+    res.render("pages/menu"), {title:"Menu"};
 });
 
 app.get("/product", (req, res) => {
-    res.render("pages/product");
+    res.render("pages/product"), {title:"Product"};
 });
 
 app.get("/product2", (req, res) => {
-    res.render("pages/product2");
+    res.render("pages/product2"), {title:"Product2"};
 });
 
 app.get("/login", (req, res) => {
-    res.render("pages/login");
+    res.render("pages/login"), {title:"Login"};
 });
 
 app.get("/register", (req, res) => {
-    res.render("pages/register");
+    res.render("pages/register"), {title:"Register"};
 });
 //-------------------------------//
 
