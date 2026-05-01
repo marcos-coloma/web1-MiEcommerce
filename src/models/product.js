b98ab848c8b758b774cfeb5dfc361ba6b942f293
Marcos <colomamarcos777@gmail.com>
2026-05-01 14:36:59 -0300

    feat: add stock status indicator and prevent adding out-of-stock products

--- src/models/product.js
+++ src/models/product.js
@@ -1,10 +1,23 @@
 const products = require("../data/products");
+const stockMap = {};
 
 const Product = {
-    getAll: () => products,
+    getAll: () => {
+        return products.map(p => {
+            if (stockMap[p.id] === undefined) {
+                stockMap[p.id] = Math.random() > 0.9 ? 0 : Math.floor(Math.random() * 10) + 1;
+            }
+
+            return {
+                ...p,
+                stock: stockMap[p.id]
+            };
+        });
+    },
 
     getById: (id) => {
-        return products.find(p => p.id === Number(id));
+        const all = Product.getAll();
+        return all.find(p => p.id === Number(id));
     }
 };
 
