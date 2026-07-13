d22fc4979cb5265b4ea7c8698b92f32a677a44f3
Marcos <colomamarcos777@gmail.com>
2026-07-13 16:24:34 -0300

    feat: add base page templates for products, categories and users

---
+++ admin/src/pages/categories/categoriesList/categoriesList.jsx
@@ -0,0 +1,10 @@
+// src/pages/Categories/CategoriesList/CategoriesList.jsx
+
+export default function CategoriesList() {
+    return (
+        <div>
+        <h1>Categorías</h1>
+        <p>Listado de categorías</p>
+        </div>
+    );
+}
---
+++ admin/src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+// src/pages/Home/Home.jsx
+
+export default function Home() {
+    return (
+        <div>
+        <h1>Dashboard</h1>
+        <p>Panel de administración</p>
+        </div>
+    );
+}
