2a91f725c55dcdcbe9c63f8cbf205447f5392a82
Marcos <colomamarcos777@gmail.com>
2026-07-13 17:33:06 -0300

    feat: add error pages structure with NotFound component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,50 @@
 // src/App.jsx
 
+import { Routes, Route } from "react-router-dom";
+
+
+// Home
+import Home from "./pages/Home/Home";
+
+// Products
+import ProductsList from "./pages/Products/ProductsList/ProductsList";
+import ProductsView from "./pages/Products/ProductsView/ProductsView";
+
+// Categories
+import CategoriesList from "./pages/Categories/CategoriesList/CategoriesList";
+import CategoriesView from "./pages/Categories/CategoriesView/CategoriesView";
+
+// Users
+import UsersList from "./pages/Users/UsersList/UsersList";
+import UsersView from "./pages/Users/UsersView/UsersView";
+
+// Errors
+import NotFound from "./errors/NotFound/NotFound";
+
 function App() {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* Products */}
+        <Route path="/products" element={<ProductsList />} />
+        <Route path="/products/:id" element={<ProductsView />} />
+
+        {/* Categories */}
+        <Route path="/categories" element={<CategoriesList />} />
+        <Route path="/categories/:id" element={<CategoriesView />} />
+
+        {/* Users */}
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/users/:id" element={<UsersView />} />
+
+        {/* Errors */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
